8727a2496413a6b8f5bbfff695ea5477b1b3b037
CatnipMountain <chriscarlos82@gmail.com>
2023-12-31 23:25:26 -0800

    Adding return book functionality

--- src/main/java/com/luv2code/springbootlibrary/service/BookService.java
+++ src/main/java/com/luv2code/springbootlibrary/service/BookService.java
@@ -111,6 +111,48 @@ public class BookService {
         }
         return shelfCurrentLoansResponses;
     };
+
+    public void returnBook (String userEmail, Long bookId) throws Exception {
+
+        Optional<Book> book = bookRepository.findById(bookId);
+
+        // verifying in the Checkout table, that we do in fact have a book checked out by this user email and book ID
+        Checkout validateCheckout = checkoutRepository.findByUserEmailAndBookId(userEmail, bookId);
+
+        if (!book.isPresent() || validateCheckout == null) {
+            throw new Exception("Book does not exist or not checkout out by user");
+        }
+
+        // increase copies available by 1
+        book.get().setCopiesAvailable(book.get().getCopiesAvailable() + 1);
+
+        // save change to Book table
+        bookRepository.save(book.get());
+        // delete from Checkout table (book returned)
+        checkoutRepository.deleteById(validateCheckout.getId());
+
+    }
+
+    public void renewLoan (String userEmail, Long bookId) throws Exception {
+
+        Checkout validateCheckout = checkoutRepository.findByUserEmailAndBookId(userEmail, bookId);
+
+        if (validateCheckout == null) {
+            throw new Exception("Book does not exist or not checkout out by user");
+        }
+
+        SimpleDateFormat sdFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+        Date d1 = sdFormat.parse(validateCheckout.getReturnDate());
+        Date d2 = sdFormat.parse(LocalDate.now().toString());
+
+        // if return date has not come yet, allow renewal of the loan by resetting to +7 days from now
+        if (d1.compareTo(d2) > 0 || d1.compareTo(d2) == 0) {
+            validateCheckout.setReturnDate(LocalDate.now().plusDays(7).toString());
+            checkoutRepository.save(validateCheckout);
+        }
+    }
+
 }
 
 
