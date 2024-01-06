a14a33252d395cde76f7e9e0a5a274e9aae2e2b3
CatnipMountain <chriscarlos82@gmail.com>
2024-01-06 15:33:48 -0800

    Messages repository/security, and pagination

--- src/main/java/com/luv2code/springbootlibrary/dao/MessageRepository.java
+++ src/main/java/com/luv2code/springbootlibrary/dao/MessageRepository.java
@@ -1,7 +1,13 @@
 package com.luv2code.springbootlibrary.dao;
 
 import com.luv2code.springbootlibrary.entity.Message;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public interface MessageRepository extends JpaRepository<Message, Long> {
+
+    // adding pagination and be able to search by user email to find all messages
+    Page<Message> findByUserEmail(@RequestParam("user_email") String userEmail, Pageable pageable);
 }
